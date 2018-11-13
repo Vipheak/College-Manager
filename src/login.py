b7f2fda138658519307a0f2c38ef4e26c90c88b3
Ed <Ed@vipheak.com>
2018-11-13 04:15:45 -0600

    Problema del Login solucionado.

--- src/login.py
+++ src/login.py
@@ -1,10 +1,12 @@
 import sys, os;
-from PyQt5.QtWidgets import QApplication, QDialog;
+from PyQt5.QtWidgets import QDialog;
 from PyQt5 import uic;
 from PyQt5.QtCore import QFile, QIODevice, QTextStream;
 from src.lib.database import DBManager;
 
 class Login(QDialog):
+    userData = False;
+
     def __init__(self):
         super().__init__();
         self.setupUi();
@@ -16,28 +18,19 @@ class Login(QDialog):
         stylesheet.open(QIODevice.ReadWrite | QIODevice.Text);
         self.setStyleSheet(QTextStream(stylesheet).readAll());
 
-        self.accept.clicked.connect(self.login);
+        self.accept.clicked.connect(self.isLogged);
         self.cancel.clicked.connect(self.close);
 
-    def login(self):
-        db = DBManager("127.0.0.1", "cm_test", "root", "", 3306); # Agregar funciones get para configuracion de base dedatos.
+    def isLogged(self):
+        db = DBManager("127.0.0.1", "cm_test", "root", "", 3306); # Agregar funciones get para configuracion de base de datos.
         db.connect();
 
         if db.isValid("usuarios", "username", self.user.text()) and db.isValid("usuarios", "password", self.password.text()):
-            userData = db.selectAllWhere("usuarios", "username = '" + self.user.text() + "'", 4);
-
-            if(userData[3] == "Admin"):
-                from src.main import MainWindow;
-                self.close();
-            if(userData[3] == "Profesor"):
-                pass
-            if(userData[3] == "Alumno"):
-                pass
+            self.userData = db.selectAllWhere("usuarios", "username = '" + self.user.text() + "'", 4);
+            self.close();
+            return True;
         else:
-            pass
+            return False;
 
-# __init__
-a = QApplication(sys.argv);
-w = Login();
-w.show();
-a.exec_();
+    def getRole(self):
+        return self.userData[3];
--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 import sys, os;
-from PyQt5.QtWidgets import QApplication, QMainWindow;
+from PyQt5.QtWidgets import QMainWindow;
 from PyQt5 import uic;
-from src.database_config import DBConfig;
 from PyQt5.QtCore import QFile, QIODevice, QTextStream;
+from src.database_config import DBConfig;
+from src.login import Login;
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -10,34 +11,37 @@ class MainWindow(QMainWindow):
         self.setupUi();
 
     def setupUi(self):
-        uic.loadUi("src/ui/main.ui", self);
+        login = Login();
+        login.exec_();
 
-        self.views.setCurrentIndex(0);
+        if login.isLogged():
+            uic.loadUi("src/ui/main.ui", self);
 
-        f = QFile("config/global.config");
-        if f.open(QIODevice.ReadWrite | QIODevice.Text):
-            isFirstLogin = str(f.readLine())[18:-3] == "True";
-            f.close();
+            if   login.getRole() == "Admin":    self.views.setCurrentIndex(0);
+            elif login.getRole() == "Profesor": self.views.setCurrentIndex(1);
+            elif login.getRole() == "Alumno":   self.views.setCurrentIndex(2);
 
-        if isFirstLogin:
-            f.remove("config/global.config");
+            f = QFile("config/global.config");
             if f.open(QIODevice.ReadWrite | QIODevice.Text):
-
-                stream = QTextStream(f);
-                stream << "is_first_login: False";
-
+                isFirstLogin = str(f.readLine())[18:-3] == "True";
                 f.close();
 
-            self.dbConfigDialog();
+            if isFirstLogin:
+                f.remove("config/global.config");
+                if f.open(QIODevice.ReadWrite | QIODevice.Text):
 
-        self.dbConfigAction.triggered.connect(self.dbConfigDialog);
+                    stream = QTextStream(f);
+                    stream << "is_first_login: False";
+
+                    f.close();
+
+                self.dbConfigDialog();
+
+            self.showFullScreen();
+            self.dbConfigAction.triggered.connect(self.dbConfigDialog);
+        else:
+            self.close();
 
     def dbConfigDialog(self):
         dbconfig = DBConfig();
         dbconfig.exec_();
-
-# __init__
-a = QApplication(sys.argv);
-w = MainWindow();
-w.showFullScreen();
-a.exec_();
