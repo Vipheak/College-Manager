efadf4c23a8301d33d839aa3fe1fb559d0c692e1
Alecas24 <38927813+Alecas24@users.noreply.github.com>
2018-11-15 14:31:04 -0600

    Add files via upload

---
+++ src/lib/encriptacion.py
@@ -0,0 +1,15 @@
+import hashlib
+import sys
+from PyQt5.QtSql import QSqlDatabase, QSqlQuery;
+import mysql.connector;
+from src.database_config import DBConfig;
+
+def make_pw_hash(password):
+    return hashlib.sha256(str.encode(password)).hexdigest()
+
+
+def check_pw_hash (password, hash):
+    if make_pw_hash(password) == hash:
+        return True
+
+    return False
